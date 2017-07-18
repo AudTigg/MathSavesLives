17273d009471654ca64976a6227813bb13df1b5a
PENguin4 <lucas.lisboa@digipen.edu>
2017-07-18 09:18:55 -0700

    fixed stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,11 @@ package com.company;
 
 import edu.digipen.Game;
 
-import java.util.logging.Level;
-
 public class Main {
 
     public static void main(String[] args) {
-        Object Game;
-        Game.initialize(500,500,60, new Level1());
+
+        Game.initialize(500, 500, 60, new Level1());
         while(Game.getQuit() == false)
             Game.update();
         Game.destroy();
